305ab6d9f0d983acbfdbc318c37d70f6ae02a838
CaptainPuppet <71453619+CaptainPuppet@users.noreply.github.com>
2022-05-24 12:06:51 -0500

    Add SQL

---
+++ Domber.sql
@@ -0,0 +1,44 @@
+/* SQL-> Structured Query Language*/
+
+CREATE DATABASE if not EXISTS uberdatabase;
+
+use uberdatabase;
+
+create table users(
+    user VARCHAR(50)PRIMARY KEY,
+    user_date DATETIME VARCHAR(50)NOT NULL,
+    
+) Engine=innoDB DEFAULT charset=utf8mb4;
+
+
+
+create table services(
+
+) Engine=innoDB DEFAULT charset=utf8mb4;
+
+
+
+create table on_status(
+
+) Engine=innoDB DEFAULT charset=utf8mb4;
+
+
+
+create table comentary(
+
+) Engine=innoDB DEFAULT charset=utf8mb4;
+
+
+
+
+create table review(
+
+) Engine=innoDB DEFAULT charset=utf8mb4;
+
+
+
+create table user_type(
+
+) Engine=innoDB DEFAULT charset=utf8mb4;
+
+
